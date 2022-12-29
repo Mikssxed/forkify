be23d02740163bf07f1f1bf0acf585f120c6dc82
Mikssxed <eksilonxd@gmail.com>
2022-12-29 16:15:17 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0bc41072-a8c3-45fd-b51e-e598ffed8d14';
+export const MODAL_CLOSE_SEC = 2.5
